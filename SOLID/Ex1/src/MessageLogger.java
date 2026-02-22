ad8cf7a71fb212c45531be8c505a87690b5cff48
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 05:17:48 +0530

    Add MessageLogger class for printing confirmations

---
+++ SOLID/Ex1/src/MessageLogger.java
@@ -0,0 +1,8 @@
+public class MessageLogger {
+    public void printConfirmation(String id, StudentRecord rec, int totalCount) {
+        System.out.println("OK: created student " + id);
+        System.out.println("Saved. Total students: " + totalCount);
+        System.out.println("CONFIRMATION:");
+        System.out.println(rec);
+    }
+}
