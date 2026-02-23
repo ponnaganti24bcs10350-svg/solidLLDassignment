ce577461a3907533d52bea80318d43c85c28dafe
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 06:05:33 +0530

    Update Exporter.java

--- SOLID/Ex5/src/Exporter.java
+++ SOLID/Ex5/src/Exporter.java
@@ -1,4 +1,12 @@
 public abstract class Exporter {
-    // implied "contract" but not enforced (smell)
     public abstract ExportResult export(ExportRequest req);
+    protected void validateRequest(ExportRequest req) {
+        if (req == null) {
+            throw new IllegalArgumentException("ExportRequest cannot be null");
+        }
+    }
+    
+    protected String safeString(String s) {
+        return s == null ? "" : s;
+    }
 }
