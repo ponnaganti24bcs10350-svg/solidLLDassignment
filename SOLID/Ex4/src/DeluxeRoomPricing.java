4736cb050de98c90ad7f0a86bd74bf1b148508dd
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 05:59:29 +0530

    Implement DeluxeRoomPricing class for pricing strategy

---
+++ SOLID/Ex4/src/DeluxeRoomPricing.java
@@ -0,0 +1,11 @@
+public class DeluxeRoomPricing implements RoomPricingStrategy {
+    @Override
+    public Money getMonthlyFee() {
+        return new Money(16000.0);
+    }
+
+    @Override
+    public int getRoomType() {
+        return LegacyRoomTypes.DELUXE;
+    }
+}
