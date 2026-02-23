c83733e8a525f2ec863e43e5f0f2904d19cc2891
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 05:46:03 +0530

    Update FileStore.java

--- SOLID/Ex2/src/FileStore.java
+++ SOLID/Ex2/src/FileStore.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class FileStore {
+public class FileStore implements InvoicePersistence {
     private final Map<String, String> files = new HashMap<>();
 
     public void save(String name, String content) { files.put(name, content); }
