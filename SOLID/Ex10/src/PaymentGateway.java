b60e421f8d115779752a994b08d9c523263dd85a
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 06:38:07 +0530

    Update PaymentGateway.java

--- SOLID/Ex10/src/PaymentGateway.java
+++ SOLID/Ex10/src/PaymentGateway.java
@@ -1,6 +1,6 @@
-public class PaymentGateway {
+public class PaymentGateway implements IPaymentGateway {
+    @Override
     public String charge(String studentId, double amount) {
-        // fake deterministic txn
         return "TXN-9001";
     }
 }
