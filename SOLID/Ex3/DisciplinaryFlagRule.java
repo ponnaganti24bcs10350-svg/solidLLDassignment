ae22c7de6fe6e6d39525b9b8744cf7dc90b89963
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 05:53:15 +0530

    Delete SOLID/Ex3/DisciplinaryFlagRule.java

--- SOLID/Ex3/DisciplinaryFlagRule.java
+++
@@ -1,9 +0,0 @@
-public class DisciplinaryFlagRule implements EligibilityRule {
-    @Override
-    public RuleResult check(StudentProfile student) {
-        if (student.disciplinaryFlag != LegacyFlags.NONE) {
-            return RuleResult.fail("disciplinary flag present");
-        }
-        return RuleResult.pass();
-    }
-}
