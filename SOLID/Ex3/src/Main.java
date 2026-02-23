7d78cb9d512f3cad53bcbfe3bd591944d593b081
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 05:55:47 +0530

    Update Main.java

--- SOLID/Ex3/src/Main.java
+++ SOLID/Ex3/src/Main.java
@@ -1,8 +1,18 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Placement Eligibility ===");
+        
+        List<EligibilityRule> rules = List.of(
+            new DisciplinaryFlagRule(),
+            new CgrRule(8.0),
+            new AttendanceRule(75),
+            new CreditsRule(20)
+        );
+        
         StudentProfile s = new StudentProfile("23BCS1001", "Ayaan", 8.10, 72, 18, LegacyFlags.NONE);
-        EligibilityEngine engine = new EligibilityEngine(new FakeEligibilityStore());
+        EligibilityEngine engine = new EligibilityEngine(new FakeEligibilityStore(), rules);
         engine.runAndPrint(s);
     }
 }
