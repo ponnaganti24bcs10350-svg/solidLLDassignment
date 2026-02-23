b9ec46d653ee13b755f3488f63d430cf8e245222
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 06:15:22 +0530

    Update EmailSender.java

--- SOLID/Ex6/src/EmailSender.java
+++ SOLID/Ex6/src/EmailSender.java
@@ -2,8 +2,7 @@ public class EmailSender extends NotificationSender {
     public EmailSender(AuditLog audit) { super(audit); }
 
     @Override
-    public void send(Notification n) {
-        // LSP smell: truncates silently, changing meaning
+    protected void doSend(Notification n) {
         String body = n.body;
         if (body.length() > 40) body = body.substring(0, 40);
         System.out.println("EMAIL -> to=" + n.email + " subject=" + n.subject + " body=" + body);
