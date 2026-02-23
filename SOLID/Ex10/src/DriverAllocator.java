cd89af35609d6c7d84bf97f70b53beaf7972aa33
ponnaganti24bcs10350-svg <ponnaganti.24bcs10350@sst.scaler.com>
2026-02-23 06:34:57 +0530

    Implement IDriverAllocator in DriverAllocator class

--- SOLID/Ex10/src/DriverAllocator.java
+++ SOLID/Ex10/src/DriverAllocator.java
@@ -1,6 +1,6 @@
-public class DriverAllocator {
+public class DriverAllocator implements IDriverAllocator {
+    @Override
     public String allocate(String studentId) {
-        // fake deterministic driver
         return "DRV-17";
     }
 }
